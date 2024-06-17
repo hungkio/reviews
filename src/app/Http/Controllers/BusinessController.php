6153ca8d9b56d5a970785ed4c6a8a00b6223f4d1
thanhnam <nguyenthanhnamx3tv@gmail.com>
2024-06-17 20:15:01 +0700

    fix upload image

--- src/app/Http/Controllers/BusinessController.php
+++ src/app/Http/Controllers/BusinessController.php
@@ -16,14 +16,14 @@ class BusinessController extends Controller
 
         $user = Auth::user();
         $business = DB::table('accounts')
-                            ->addSelect('business_name')
-                            ->addSelect('website_url')
-                            ->addSelect('logo')
-                            ->addSelect('color')
-                            ->addSelect('from_email')
-                            ->addSelect('reply_to_email')
-                            ->where('accounts_id',$user->account_id)
-                            ->first();
+            ->addSelect('business_name')
+            ->addSelect('website_url')
+            ->addSelect('logo')
+            ->addSelect('color')
+            ->addSelect('from_email')
+            ->addSelect('reply_to_email')
+            ->where('accounts_id', $user->account_id)
+            ->first();
         return view('pages/apps.business.index', compact('business'));
     }
 
@@ -31,50 +31,41 @@ class BusinessController extends Controller
     {
         $user = Auth::user();
 
-        $request->validate([
-            'business_name' => 'required|string|max:255',
-            'logo' => 'nullable|image|max:2048',
-            'website_url' => 'required|url',
-            'color' => 'nullable|string|max:7',
-            'from_email' => 'required|email',
-            'reply_to_email' => 'nullable|email'
-        ]);
-
         $data = $request->all();
 
-        if($request->hasFile('logo')){
+        if ($request->hasFile('logo')) {
             try {
                 $storedPath = Storage::putFile('public', $request->file('logo'));
                 $data['logo'] = Storage::url($storedPath);
             } catch (Exception $e) {
             }
-        } 
+        }
         $check_exits = DB::table('accounts')
-                       ->where('accounts_id',$user->account_id)
-                       ->first();
+            ->where('accounts_id', $user->account_id)
+            ->first();
 
         $data_insert = [
-            'business_name' => $data['business_name'],
-            'website_url' => $data['website_url'],
-            'logo' => $data['logo'],
-            'color' => $data['color'],
-            'from_email' => $data['from_email'],
-            'reply_to_email' => $data['reply_to_email'],
-            'created_at' => Carbon::now(),
+            'business_name' => $data['business_name'] ?? $check_exits->business_name ?? '',
+            'website_url' => $data['website_url'] ?? $check_exits->website_url ?? '',
+            'logo' => $data['logo'] ?? $check_exits->logo ?? '',
+            'color' => $data['color'] ?? $check_exits->color ?? '',
+            'from_email' => $data['from_email'] ?? $check_exits->from_email ?? '',
+            'reply_to_email' => $data['reply_to_email'] ?? $check_exits->reply_to_email ?? '',
             'updated_at' => Carbon::now(),
         ];
 
+
         try {
-            if(!$check_exits){
+            if (!$check_exits) {
                 $data_insert['created_at'] = Carbon::now();
                 DB::table('accounts')->insert($data_insert);
-            }else{
-                DB::table('accounts')->where('accounts_id',$user->account_id)->update($data_insert);
+            } else {
+                DB::table('accounts')->where('accounts_id', $user->account_id)->update($data_insert);
             }
         } catch (Exception $e) {
             print_r($e);
         }
-        
+
         return redirect()->back()->with('success', 'Business information saved successfully!');
     }
 }
