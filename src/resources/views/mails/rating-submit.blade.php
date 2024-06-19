75595fc3fb4da844f0c0829204bab5abdb9decd1
thanhnam <nguyenthanhnamx3tv@gmail.com>
2024-06-19 16:39:50 +0700

    handle send email

--- src/resources/views/mails/rating-submit.blade.php
+++ src/resources/views/mails/rating-submit.blade.php
@@ -5,166 +5,313 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Star Rating Form</title>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <style>
-        * {
-            font-family: 'Montserrat', sans-serif !important;
-        }
-
         body {
+            text-align: center;
+            line-height: 80vh;
+            font-family: 'Roboto';
             margin: 0;
             padding: 0;
         }
 
-        .rating {
-            display: flex;
-            flex-direction: row;
-            font-size: 1.5rem;
-            justify-content: center!important;
-            align-items: center!important;
-            width: 100%!important;
-            text-align: center!important
+        @keyframes confetti-slow {
+            0% {
+                transform: translate3d(0, 0, 0) rotateX(0) rotateY(0);
+            }
+
+            100% {
+                transform: translate3d(25px, 105vh, 0) rotateX(360deg) rotateY(180deg);
+            }
         }
 
-        .rating label {
-            font-size: 1.5rem;
-            color: #ffc700;
-            cursor: pointer;
+        @keyframes confetti-medium {
+            0% {
+                transform: translate3d(0, 0, 0) rotateX(0) rotateY(0);
+            }
+
+            100% {
+                transform: translate3d(100px, 105vh, 0) rotateX(100deg) rotateY(360deg);
+            }
         }
 
-        .rating input {
-            margin-top: 3px;
+        @keyframes confetti-fast {
+            0% {
+                transform: translate3d(0, 0, 0) rotateX(0) rotateY(0);
+            }
+
+            100% {
+                transform: translate3d(-50px, 105vh, 0) rotateX(10deg) rotateY(250deg);
+            }
         }
 
         .container {
-            width: 100%;
-            height: auto;
-            padding: 50px 0;
+            width: 100vw;
+            height: 100vh;
+            background: #ffffff;
+            border: 1px solid white;
+            display: fixed;
+            top: 0px;
         }
 
-        .form-wrapper {
-            width: 100%;
-            background: white;
-            max-width: 568px;
-            height: auto;
-            margin: 0 auto;
-            text-align: center;
-            box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-            padding: 20px 15px;
-            border-radius: 10px;
+        .confetti-container {
+            perspective: 700px;
+            position: absolute;
+            overflow: hidden;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
         }
 
-        .form-wrapper h1 {
-            color: #443c68;
+        .confetti {
+            position: absolute;
+            z-index: 1;
+            top: -10px;
+            border-radius: 0%;
+
+            &--animation-slow {
+                animation: confetti-slow 2.25s linear 1 forwards;
+            }
+
+            &--animation-medium {
+                animation: confetti-medium 1.75s linear 1 forwards;
+            }
+
+            &--animation-fast {
+                animation: confetti-fast 1.25s linear 1 forwards;
+            }
         }
 
-        .feedback-wrapper {
-            width: 100%;
-            text-align: left;
+
+
+
+
+        /* Checkmark */
+        .checkmark-circle {
+            width: 150px;
+            height: 150px;
+            position: relative;
+            display: inline-block;
+            vertical-align: top;
+            margin-left: auto;
+            margin-right: auto;
         }
 
-        .feedback-wrapper h3 {
-            text-align: left;
-            font-weight: 600;
+        .checkmark-circle .background {
+            width: 150px;
+            height: 150px;
+            border-radius: 50%;
+            background: #00C09D;
+            position: absolute;
         }
 
-        .feedback-wrapper textarea {
-            font-family: 'Montserrat', sans-serif;
-            width: 100%;
-            height: 120px;
-            margin-bottom: 20px;
-            margin-top: 10px;
-            padding: 15px;
-            border: none;
-            background-color: #0e77ff1a;
-            outline: none;
+        .checkmark-circle .checkmark {
             border-radius: 5px;
-            resize: vertical;
-            font-size: 0.7rem;
-            box-sizing: border-box;
         }
 
-        .btn-submit {
-            font-family: 'Montserrat', sans-serif;
-            padding: 10px 55px;
-            border: none;
-            border-radius: 20px;
-            background-color: #0e77ff;
-            color: white;
-            font-size: 0.8rem;
+        .checkmark-circle .checkmark.draw:after {
+            -webkit-animation-delay: 100ms;
+            -moz-animation-delay: 100ms;
+            animation-delay: 100ms;
+            -webkit-animation-duration: 3s;
+            -moz-animation-duration: 3s;
+            animation-duration: 3s;
+            -webkit-animation-timing-function: ease;
+            -moz-animation-timing-function: ease;
+            animation-timing-function: ease;
+            -webkit-animation-name: checkmark;
+            -moz-animation-name: checkmark;
+            animation-name: checkmark;
+            -webkit-transform: scaleX(-1) rotate(135deg);
+            -moz-transform: scaleX(-1) rotate(135deg);
+            -ms-transform: scaleX(-1) rotate(135deg);
+            -o-transform: scaleX(-1) rotate(135deg);
+            transform: scaleX(-1) rotate(135deg);
+            -webkit-animation-fill-mode: forwards;
+            -moz-animation-fill-mode: forwards;
+            animation-fill-mode: forwards;
+        }
+
+        .checkmark-circle .checkmark:after {
+            opacity: 1;
+            height: 75px;
+            width: 37.5px;
+            -webkit-transform-origin: left top;
+            -moz-transform-origin: left top;
+            -ms-transform-origin: left top;
+            -o-transform-origin: left top;
+            transform-origin: left top;
+            border-right: 15px solid white;
+            border-top: 15px solid white;
+            border-radius: 2.5px !important;
+            content: '';
+            left: 25px;
+            top: 75px;
+            position: absolute;
+        }
+
+        @-webkit-keyframes checkmark {
+            0% {
+                height: 0;
+                width: 0;
+                opacity: 1;
+            }
+
+            20% {
+                height: 0;
+                width: 37.5px;
+                opacity: 1;
+            }
+
+            40% {
+                height: 75px;
+                width: 37.5px;
+                opacity: 1;
+            }
+
+            100% {
+                height: 75px;
+                width: 37.5px;
+                opacity: 1;
+            }
+        }
+
+        @-moz-keyframes checkmark {
+            0% {
+                height: 0;
+                width: 0;
+                opacity: 1;
+            }
+
+            20% {
+                height: 0;
+                width: 37.5px;
+                opacity: 1;
+            }
+
+            40% {
+                height: 75px;
+                width: 37.5px;
+                opacity: 1;
+            }
+
+            100% {
+                height: 75px;
+                width: 37.5px;
+                opacity: 1;
+            }
+        }
+
+        @keyframes checkmark {
+            0% {
+                height: 0;
+                width: 0;
+                opacity: 1;
+            }
+
+            20% {
+                height: 0;
+                width: 37.5px;
+                opacity: 1;
+            }
+
+            40% {
+                height: 75px;
+                width: 37.5px;
+                opacity: 1;
+            }
+
+            100% {
+                height: 75px;
+                width: 37.5px;
+                opacity: 1;
+            }
+        }
+
+
+
+        .submit-btn {
+            height: 45px;
+            width: 200px;
+            font-size: 15px;
+            background-color: #00c09d;
+            border: 1px solid #00ab8c;
+            color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px 0 rgba(87, 71, 81, .2);
             cursor: pointer;
+            transition: all 2s ease-out;
+            transition: all .2s ease-out;
         }
 
-        @media (max-width: 600px) {
-            .rating {
-                font-size: 1rem;
-                padding-right: 0;
-            }
-
-            .rating label {
-                font-size: 1rem;
-            }
-
-            .form-wrapper {
-                padding: 15px;
-            }
-
-            .feedback-wrapper textarea {
-                font-size: 0.8rem;
-                height: 100px;
-            }
-
-            .btn-submit {
-                padding: 10px 30px;
-                font-size: 0.7rem;
-            }
+        .submit-btn:hover {
+            background-color: #2ca893;
+            transition: all .2s ease-out;
         }
     </style>
 </head>
 <?php
 $maxStars = 5;
 $emojis = ["😡", "😟", "😐", "😊", "😍"];
-$rating_style = isset($rating_style) ? $rating_style: "stars";
+$rating_style = isset($rating_style) ? $rating_style : "stars";
 ?>
 
 <body>
-<div class="container" style="background-color: <?php echo $background_color; ?>;">
-<form action="{{ route('webform.store') }}" method="POST" enctype="multipart/form-data" class="form-wrapper">
-        @csrf
-        <input type="hidden" name="payment_id" value="{{$payment_id ?? ''}}">
-        <input type="hidden" name="payment_intent_id" value="{{$payment_intent_id ?? ''}}">
-        <h1>Rate Us</h1>
-        <?php echo $email_body; ?>
-        <div class="rating" style="justify-content: center!important; align-items: center!important; width: 100%!important; text-align: center!important">
-            <?php
-            if ($rating_style == 'stars') {
-                for ($i = 1; $i <= $maxStars; $i++) {
-                    echo '<div class="rating" style="justify-content: center!important; align-items: center!important; width: 100%!important; text-align: center!important">';
-                    echo '<input type="radio" id="star' . $i . '" name="star" value="' . $i . '" ' . ($star == $i ? 'checked' : '') . '>';
-                    for ($j = 1; $j <= $i; $j++) {
-                        echo '<label for="star' . $i . '" title="' . $i . ' star">&#9733;</label>';
-                    }
-                    echo '</div>';
-                }
-            } else {
-                for ($i = 1; $i <= $maxStars; $i++) {
-                    echo '<div class="rating" style="justify-content: center!important; align-items: center!important; width: 100%!important; text-align: center!important">';
-                    echo '<input type="radio" id="star' . $i . '" name="star" value="' . $i . '" ' . ($star == $i ? 'checked' : '') . '>';
-                    echo '<label for="star' . $i . '" title="' . $i . ' star">' . $emojis[$i - 1] . '</label>';
-                    echo '</div>';
-                }
-            }
-            ?>
-        </div>
+    <div class="js-container container" style="top:0px !important;">
+        <h1>
+            Thank you for your review
+        </h1>
+    </div>
 
-        <div class="feedback-wrapper">
-            <h3 for="review">Feedback:</h3>
-            <textarea id="review" placeholder="Enter your feedback" value="{{$review ?? ''}}" name="review" rows="4" cols="50">{{ $review ?? '' }}</textarea><br>
-        </div>
-        <button class="btn-submit" type="submit">Submit</button>
-    </form>
-</div>
+
+
+    <div class="container" style="background-color: <?php echo $background_color; ?>; display: none">
+        <form action="{{ route('webform.store') }}" method="POST" enctype="multipart/form-data" class="form-wrapper">
+            @csrf
+            <input type="hidden" name="payment_id" value="{{$payment_id ?? ''}}">
+            <input type="hidden" name="payment_intent_id" value="{{$payment_intent_id ?? ''}}">
+            <h1>Rate Us</h1>
+            <?php echo $email_body; ?>
+            <div class="rating" style="justify-content: center!important; align-items: center!important; width: 100%!important; text-align: center!important">
+                <?php
+                if ($rating_style == 'stars') {
+                    for ($i = 1; $i <= $maxStars; $i++) {
+                        echo '<div class="rating" style="justify-content: center!important; align-items: center!important; width: 100%!important; text-align: center!important">';
+                        echo '<input type="radio" id="star' . $i . '" name="star" value="' . $i . '" ' . ($star == $i ? 'checked' : '') . '>';
+                        for ($j = 1; $j <= $i; $j++) {
+                            echo '<label for="star' . $i . '" title="' . $i . ' star">&#9733;</label>';
+                        }
+                        echo '</div>';
+                    }
+                } else {
+                    for ($i = 1; $i <= $maxStars; $i++) {
+                        echo '<div class="rating" style="justify-content: center!important; align-items: center!important; width: 100%!important; text-align: center!important">';
+                        echo '<input type="radio" id="star' . $i . '" name="star" value="' . $i . '" ' . ($star == $i ? 'checked' : '') . '>';
+                        echo '<label for="star' . $i . '" title="' . $i . ' star">' . $emojis[$i - 1] . '</label>';
+                        echo '</div>';
+                    }
+                }
+                ?>
+            </div>
+
+            <div class="feedback-wrapper">
+                <h3 for="review">Feedback:</h3>
+                <textarea id="review" placeholder="Enter your feedback" value="{{$review ?? ''}}" name="review" rows="4" cols="50">{{ $review ?? '' }}</textarea><br>
+            </div>
+            <button class="btn-submit" type="submit" id="submit-review">Submit</button>
+        </form>
+    </div>
 </body>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var submitButton = document.getElementById("submit-review");
+        if (submitButton) {
+            setTimeout(function() {
+                submitButton.click();
+            }, 2000);
+        }
+    });
+</script>
 
 </html>
